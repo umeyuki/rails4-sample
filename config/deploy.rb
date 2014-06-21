2f24425b0e96b3bda2a4e92a5f037da9a71aefde
umeyuki <umeyuki1326+github@gmail.com>
2014-06-21 23:28:08 +0900

    update

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,6 +11,8 @@ set :keep_releases, 5
 
 set :linked_dirs, (fetch(:linked_dirs) + ['tmp/pids'])
 
+set :default_env, { path: '/home/umeyuki/local/ruby-2.1.1/bin:$PATH' }
+
 set :unicorn_rack_env, "none"
 set :unicorn_config_path, 'config/unicorn.rb'
 
